554ec1d70047d92239edc231f4069080b6eabe48
donchokella <ali.alparslan@outlook.com>
2023-01-20 14:05:43 +0300

    PlayerShip moves and fires, bullet moves and is destroyed after a while

---
+++ Assets/Scripts/FriendlyBullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyBullet : MonoBehaviour
+{
+    private float speed = 10f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+}
